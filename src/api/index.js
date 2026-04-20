7ba3129b2ad54a2fa276636f9865dac8f47eb675
Naitik06052006 <naitik0605@gmail.com>
2026-04-20 14:49:44 +0530

    API ISSUES

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: `${import.meta.env.VITE_API_URL}/api` })
+;
 
 // Attach JWT token to every request
 API.interceptors.request.use((config) => {
